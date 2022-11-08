f33c81db54ec9b362524092473bb8566fe1b9c7c
Yuxin Zhou <yuxin_zhou@ucsb.edu>
2022-11-08 00:38:43 -0800

    rerun R12 and R13

--- main.m
+++ main.m
@@ -1,9 +1,10 @@
 poolobj = parpool('local', 40);
 fprintf('Number of workers: %g\n', poolobj.NumWorkers);
 
-inputFile = 'R11';
+inputFile = 'R13';
 
 % inputMode = 'age_model_construction';
 inputMode = 'stack_construction';
-
+tic
 BIGMACS(inputFile,inputMode,'show');
+toc
