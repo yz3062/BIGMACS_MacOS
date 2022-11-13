a4250b72bf4f1861e7b42c4047e793d33c73069f
Yuxin Zhou <yuxin_zhou@ucsb.edu>
2022-11-13 02:16:27 -0800

    pod

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
-%poolobj = parpool('local', 36);
-%fprintf('Number of workers: %g\n', poolobj.NumWorkers);
+poolobj = parpool('local', 40);
+fprintf('Number of workers: %g\n', poolobj.NumWorkers);
 
-inputFile = 'R15';
+inputFile = 'R11';
 
 % inputMode = 'age_model_construction';
 inputMode = 'stack_construction';
