6a86fc23bac77a17f52ce4f34d28e103ada63c76
Yuxin Zhou <yuxin_zhou@ucsb.edu>
2023-02-15 12:25:31 -0700

    R30-31 output

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 poolobj = parpool('local', 36);
 fprintf('Number of workers: %g\n', poolobj.NumWorkers);
 
-inputFile = 'R29';
+inputFile = 'R31';
 
 % inputMode = 'age_model_construction';
 inputMode = 'stack_construction';
