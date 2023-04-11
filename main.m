e72228270bd1331817c202260169cb408854c28e
Yuxin Zhou <yuxin_zhou@ucsb.edu>
2023-04-11 16:56:13 -0700

    R56 output

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
-poolobj = parpool('local', 36);
+poolobj = parpool('local', 40);
 fprintf('Number of workers: %g\n', poolobj.NumWorkers);
 
-inputFile = 'R55';
+inputFile = 'R56';
 
 inputMode = 'stack_construction';
 
