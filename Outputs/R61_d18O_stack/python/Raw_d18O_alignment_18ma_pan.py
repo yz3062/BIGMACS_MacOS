193db7ee27422ed0a42df20316115fda7f09eda6
Yuxin Zhou <yzhou@ldeo.columbia.edu>
2023-07-21 11:38:16 -0700

    R62 input

--- Outputs/R61_d18O_stack/python/Raw_d18O_alignment_18ma_pan.py
+++ Outputs/R61_d18O_stack/python/Raw_d18O_alignment_18ma_pan.py
@@ -86,6 +86,25 @@ columns = [n for n, v in ndata.items()]
 df_Atlantic = pd.DataFrame(np.concatenate([ndata[c] for c in columns], axis=1),
                   columns=columns)
 
+# U1308 data
+stack_path = '../../R60_d18O_stack/results.mat'
+mat = scipy.io.loadmat(stack_path)
+
+mdata = mat['summary']  # variable in mat file
+mdtype = mdata.dtype  # dtypes of structures are "unsized objects"
+# * SciPy reads in structures as structured NumPy arrays of dtype object
+# * The size of the array is the size of the structure array, not the number
+#   elements in any particular field. The shape defaults to 2-dimensional.
+# * For convenience make a dictionary of the data using the names from dtypes
+# * Since the structure has only one element, but is 2-D, index it at [0, 0]
+ndata = {n: mdata[n] for n in mdtype.names}
+# Reconstruct the columns of the data table from just the time series
+# Use the number of intervals to test if a field is a column or metadata
+columns = [n for n, v in ndata.items()]
+# now make a data frame, setting the time stamps as the index
+df_U1308 = pd.DataFrame(np.concatenate([ndata[c] for c in columns], axis=1),
+                  columns=columns)
+
 Atlantic_path = '../stack.txt'
 Atlantic_stack = pd.read_table(Atlantic_path, delimiter=' ')
 Atlantic_stack = Atlantic_stack.groupby('age(kyr)').mean()
@@ -124,29 +143,45 @@ for core_name in pacific_core_names:
     axes_index += 1
     
 # list of cores to plot from the Atlantic
-atlantic_core_names = ['U1308', 'ODP926',
+atlantic_core_names = ['U1308','ODP926_1',
                        'ODP928', '929_LR04age', '607_LR04age',
                        '659_LR04age', 'ODP1267', 'ODP625']
 
-atlantic_core_display_names = ['U1308', 'ODP 926',
+atlantic_core_display_names = ['U1308','ODP 926',
                                r'$ODP\ 928$', r'$ODP\ 929$', r'$DSDP\ 607$',
                                r'$ODP\ 659$', 'ODP 1267', 'ODP 625']
 
 for core_name in atlantic_core_names:
-    d18O = ma.fix_invalid(df_Atlantic[df_Atlantic['name']==core_name]['d18O'].iloc[0]).mean(axis=1)
-    shift = ma.squeeze(df_Atlantic[df_Atlantic['name']==core_name]['d18O_shift'])
-    scale = ma.squeeze(df_Atlantic[df_Atlantic['name']==core_name]['d18O_scale'])
-    d18O = (d18O - shift) / scale # from BIGMACS saveFigures.m
-    age = ma.squeeze(df_Atlantic[df_Atlantic['name']==core_name]['median'])
-    age = age[~d18O.mask.transpose()]
-    d18O = d18O[~d18O.mask]
-    axes[axes_index].plot(age, d18O, 'o-', alpha=1, color='C5',markersize=2)
-    axes[axes_index].plot(LR04.index/1000, LR04, color='k', alpha=0.5)
-    axes[axes_index].set_ylim(2.7, 4.6)
-    axes[axes_index].invert_yaxis()
-    axes[axes_index].text(2130, 3.8,
-                          atlantic_core_display_names[axes_index-6],
-                          color='C5')
+    if axes_index == 6:
+        d18O = ma.fix_invalid(df_U1308[df_U1308['name']==core_name]['d18O'].iloc[0]).mean(axis=1)
+        shift = ma.squeeze(df_U1308[df_U1308['name']==core_name]['d18O_shift'])
+        scale = ma.squeeze(df_U1308[df_U1308['name']==core_name]['d18O_scale'])
+        d18O = (d18O - shift) / scale # from BIGMACS saveFigures.m
+        age = ma.squeeze(df_U1308[df_U1308['name']==core_name]['median'])
+        age = age[~d18O.mask.transpose()]
+        d18O = d18O[~d18O.mask]
+        axes[axes_index].plot(age, d18O, 'o-', alpha=1, color='C5',markersize=2)
+        axes[axes_index].plot(LR04.index/1000, LR04, color='k', alpha=0.5)
+        axes[axes_index].set_ylim(2.7, 4.6)
+        axes[axes_index].invert_yaxis()
+        axes[axes_index].text(2130, 3.8,
+                              atlantic_core_display_names[axes_index-6],
+                              color='C5')
+    else:
+        d18O = ma.fix_invalid(df_Atlantic[df_Atlantic['name']==core_name]['d18O'].iloc[0]).mean(axis=1)
+        shift = ma.squeeze(df_Atlantic[df_Atlantic['name']==core_name]['d18O_shift'])
+        scale = ma.squeeze(df_Atlantic[df_Atlantic['name']==core_name]['d18O_scale'])
+        d18O = (d18O - shift) / scale # from BIGMACS saveFigures.m
+        age = ma.squeeze(df_Atlantic[df_Atlantic['name']==core_name]['median'])
+        age = age[~d18O.mask.transpose()]
+        d18O = d18O[~d18O.mask]
+        axes[axes_index].plot(age, d18O, 'o-', alpha=1, color='C5',markersize=2)
+        axes[axes_index].plot(LR04.index/1000, LR04, color='k', alpha=0.5)
+        axes[axes_index].set_ylim(2.7, 4.6)
+        axes[axes_index].invert_yaxis()
+        axes[axes_index].text(2130, 3.8,
+                              atlantic_core_display_names[axes_index-6],
+                              color='C5')
     axes_index += 1
 
 #%% beautification
@@ -182,5 +217,5 @@ axes[-1].set_xlabel('Age (ka BP)')
 
 fig.set_size_inches(6,10)
 
-plt.savefig('Raw_d18O_alignment_18ma_pan.png', dpi=700)
+plt.savefig('Raw_d18O_alignment_18ma_pan_U1308_target.png', dpi=700)
 # plt.savefig('Raw_d18O_alignment_18ma_pan.pdf')
--- Outputs/R61_d18O_stack/python/stack_comparison.py
+++ Outputs/R61_d18O_stack/python/stack_comparison.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 23 13:03:36 2022
+Created on Wed Jul 19 15:38:16 2023
 
 @author: zhou
 """
@@ -27,21 +27,28 @@ stack_path = '../stack.txt'
 # stack = scipy.io.loadmat(stack_path)
 stack = pd.read_table(stack_path, delimiter=' ')
 
-LR04 = LR04_fetching_func.fetch_d18O()
+stack_path = '../../R60_d18O_stack/stack.txt'
+# stack = scipy.io.loadmat(stack_path)
+stack_R60 = pd.read_table(stack_path, delimiter=' ')
+
+# LR04 = LR04_fetching_func.fetch_d18O()
 
 #%% plot
 fig, axes = plt.subplots(1,1, sharex=False)
 
-axes.plot(stack['age(kyr)'], stack['mean(permil)'], label='BIGMACS')
-axes.fill_between(stack['age(kyr)'],
-                  stack['mean(permil)']+2*stack['sigma(permil)'],
-                  stack['mean(permil)']-2*stack['sigma(permil)'],
-                  alpha=0.3,
-                  label='BIGMACS 2sigma')
+axes.plot(stack['age(kyr)'], stack['mean(permil)'], label='Target: U1308')
+axes.plot(stack_R60['age(kyr)'], stack_R60['mean(permil)'], label='Target: LR04')
+# axes.fill_between(stack['age(kyr)'],
+#                   stack['mean(permil)']+2*stack['sigma(permil)'],
+#                   stack['mean(permil)']-2*stack['sigma(permil)'],
+#                   alpha=0.3,
+#                   label='BIGMACS 2sigma')
 
-axes.plot(LR04.index/1000, LR04, label='LRO4', color='k')
+# axes.plot(LR04.index/1000, LR04, label='LRO4', color='k')
 axes.set_xlim((stack['age(kyr)'].min(),stack['age(kyr)'].max()))
 axes.invert_yaxis()
+
+axes.axvspan(1800,1900,color='lightgray')
 
 axes.legend()
 
@@ -50,4 +57,4 @@ axes.set_xlabel('Age (ka BP)')
 
 fig.set_size_inches(6,4)
 
-# plt.savefig('Stack_LR04.png', dpi=500)
+plt.savefig('Stack_comparison.png', dpi=500)
