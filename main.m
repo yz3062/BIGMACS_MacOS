efc823d01bfffb152521e8197aec2827ba05a01f
Yuxin Zhou <yzhou@ldeo.columbia.edu>
2023-07-27 17:11:01 -0700

    R65 input revised to avoid error

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
-poolobj = parpool('local', 36);
-fprintf('Number of workers: %g\n', poolobj.NumWorkers);
+% poolobj = parpool('local', 36);
+% fprintf('Number of workers: %g\n', poolobj.NumWorkers);
 
-inputFile = 'R64';
+inputFile = 'R65';
 
 inputMode = 'stack_construction';
 
