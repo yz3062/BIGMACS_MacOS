c1aed54a7b1da6841da953f9cec0ad67d51b8e8f
Yuxin Zhou <yuxin_zhou@ucsb.edu>
2023-07-28 16:55:51 -0600

    R65-66 output

---
+++ main_R66.m
@@ -0,0 +1,8 @@
+poolobj = parpool('local', 36);
+fprintf('Number of workers: %g\n', poolobj.NumWorkers);
+
+inputFile = 'R66';
+
+inputMode = 'stack_construction';
+
+BIGMACS(inputFile,inputMode,'show');
