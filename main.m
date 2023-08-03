2c7ea976faeab0ce3ec175d2d4566dd42636d0e1
Yuxin Zhou <yzhou@ldeo.columbia.edu>
2023-08-03 16:03:09 -0700

    R73 revised again

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
-poolobj = parpool('local', 40);
-fprintf('Number of workers: %g\n', poolobj.NumWorkers);
+% poolobj = parpool('local', 40);
+% fprintf('Number of workers: %g\n', poolobj.NumWorkers);
 
-inputFile = 'R72';
+inputFile = 'R73';
 
 inputMode = 'stack_construction';
 
