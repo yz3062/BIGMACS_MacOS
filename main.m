460c2a26bec266dba12fedddd4e95e68ea956f94
Yuxin Zhou <yuxin_zhou@ucsb.edu>
2023-10-10 11:05:07 -0600

    R80 output

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
 poolobj = parpool('local', 36);
 fprintf('Number of workers: %g\n', poolobj.NumWorkers);
 
-inputFile = 'R77';
+inputFile = 'R80';
 
-inputMode = 'age_model_construction';
+inputMode = 'stack_construction';
 
 BIGMACS(inputFile,inputMode,'show');
