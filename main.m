dfed4970db5b8fef257c85608c9f42266fc5c8d9
Yuxin Zhou <yuxin_zhou@ucsb.edu>
2024-01-24 15:31:48 -0800

    local changes

--- main.m
+++
@@ -1,8 +0,0 @@
-poolobj = parpool('local', 40);
-fprintf('Number of workers: %g\n', poolobj.NumWorkers);
-
-inputFile = 'R85';
-
-inputMode = 'stack_construction';
-
-BIGMACS(inputFile,inputMode,'show');
