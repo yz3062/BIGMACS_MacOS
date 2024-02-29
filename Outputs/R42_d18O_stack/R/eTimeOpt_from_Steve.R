10c801bd69ae185eda4ced71440a59904abd473b
Yuxin Zhou <yzhou@ldeo.columbia.edu>
2024-02-28 22:04:59 -0800

    R

--- Outputs/R42_d18O_stack/R/eTimeOpt_from_Steve.R
+++ Outputs/R42_d18O_stack/R/eTimeOpt_from_Steve.R
@@ -2,14 +2,14 @@
 library(astrochron)
 library(readxl)
 
-ex <- read_excel('Stack_untuned_uniform_age_exclusion_expanded_1172_removed_Hobart2023.xlsx')
+dat <- read_excel('Stack_untuned_uniform_age_exclusion_expanded_1172_removed_Hobart2023.xlsx')
 
 # conduct an initial time-frequency analysis using 
 #  evolutive harmonic analysis (eha).
 #  use option 'genplot=4', which provides a useful normalization
 #  for visualizing the stability of the bedding cycles and
 #  evaluating changes in the relative dominance
-eha(ex,win=500,fmax=0.1,ydir=-1,genplot=4,pl=2,ylab="Age (ka)",xlab="Frequency (cycles/ka)")
+eha(dat,win=500,fmax=0.1,ydir=-1,genplot=4,pl=2,ylab="Age (ka)",xlab="Frequency (cycles/ka)")
 
 #  the astronomical target comes from Laskar et al. (2004)
 # targetP contains the dominant obliquity term.
@@ -17,7 +17,7 @@ targetP=c(41.08463)
 # targetE contains the amplitude modulation terms for obliquity.
 targetE=c(174.8252,106.6098,97.08738)
 #  we will use a 400 ka window.
-res=eTimeOpt(ex,targetP=targetP,targetE=targetE,sedmin=80,sedmax=120,win=400,fitModPwr=F,flow=1/70,fhigh=1/25, roll=10^9, output=1,ydir=-1)			
+res=eTimeOpt(dat,targetP=targetP,targetE=targetE,sedmin=80,sedmax=120,win=400,fitModPwr=F,flow=1/70,fhigh=1/25, roll=10^9, output=1,ydir=-1)			
 
 # extract the optimal sedimentation rates from the eTimeOpt results
 #  we will focus on the spectral power fit (middle panel in above figure),
@@ -30,10 +30,32 @@ control=sedrate2time(sedrates)
 # use the eTimeOpt depth-time map to 'tune' the oxygen isotope data.
 #  note that we are extrapolating the sedimentation rate for the
 #  lowermost and uppermost 200 ka of the data set.
-dat2=tune(ex,control=control,extrapolate=T)
+dat2=tune(dat,control=control,extrapolate=T)
+
+# anchor the floating time scale to the present
+dat2[1]=dat2[1]-dat2[1,1]
 
 # interpolate to even sampling grid (use median sampling interval)
 dat2_lin=linterp(dat2)
 
 # conduct a new eha analysis on the tuned record
 eha(dat2_lin,win=500,fmax=0.1,ydir=-1,genplot=4,pl=2,ylab="Age (ka)",xlab="Frequency (cycles/ka)")
+
+# compare oxygen isotope data on depth-derived and eTimeOpt timescales
+plot(dat2_lin,type="l",col="red",xlab="Age (ka)",ylab="d18O", main="red=eTimeOpt timescale        black=depth-derived timescale"); lines(dat)
+
+# this is a histogram of the temporal adjustments between the eTimeOpt and depth-derived timescales
+hist(dat2[,1]-dat[,1],xlab="ka",main="Temporal adjustments")
+
+# this is a plot of the difference between the eTimeOpt and depth-derived timescales at each datum
+plot(dat2[,1],dat2[,1]-dat[,1],xlab="eTimeOpt timescale (ka)",ylab="eTimeOpt - depth derived timescale (ka)",main="Temporal adjustments")
+
+plot(dat2[,1],dat2[,1]-dat[,1]-10.0614,xlab="eTimeOpt timescale (ka)",ylab="eTimeOpt - depth derived timescale (ka)",main="Temporal adjustments"); abline(h=0,lty=2)
+
+dat3=tune(dat,control=control,extrapolate=F)
+# anchor the floating time scale so that the youngest (non extrapolated) value is 200 ka
+dat3[1]=dat3[1]+dat[201,1]
+# interpolate to even sampling grid (use median sampling interval)
+dat3_lin=linterp(dat3)
+# compare oxygen isotope data on depth-derived and eTimeOpt timescales
+plot(dat3_lin,type="l",col="red",lwd=1.5,xlab="Age (ka)",ylab="d18O", main="red=eTimeOpt timescale        black=depth-derived timescale"); lines(dat)
